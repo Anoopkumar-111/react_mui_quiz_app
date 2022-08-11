a5f284ddbbf1e94ae1575eb5c9e206261f1d4f47
Anoopkumar-111 <anoopkumarshukla111@gmail.com>
2022-08-11 12:28:49 +0530

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made with ♥ by{" "}
+      <a
+        href="https://github.com/Anoopkumar-111"
+        style={{ cursor: "pointer" }}
+      >
+        Anoopkumar
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
